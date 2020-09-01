18b450c11da2d9869ee795d1b34cf83dc8edd6f0
OscarBorges15 <51797077+OscarBorges15@users.noreply.github.com>
2020-08-31 21:18:07 -0300

    feat : inicio publi Mobile

--- src/pages/Publicacoes/index.js
+++ src/pages/Publicacoes/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import './styles.css';
+import Footer from '../../components/Footer/index';
+import Article from '../../components/Article/index';
+import './styles.css';
+import FaleConosco from '../../components/Fale-conosco/index.js';
 
 export default function Publicacoes() {
   return (
-    <div>
-      
+    <div id="container-Publi" className="Publi">
+      <h1 id="title-page-publi"> Publicações Académicas </h1>
+        <div id="publi-text-container">
+          <p id="publi-text">
+          Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim
+          Lorim ypsolomm Lorim Lorim ypsolomm LorimLorim ypsolomm Lorim Lorim ypsolomm Lorim
+           
+         </p>
+        </div>
+
+        <Article 
+        title="Publicações 1"
+        description=" Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim
+        Lorim ypsolomm Lorim Lorim ypsolomm LorimLorim ypsolomm Lorim Lorim ypsolomm Lorim" 
+        ></Article>
+
+        <Article 
+        title="Publicações 2"
+        description=" Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim
+        Lorim ypsolomm Lorim Lorim ypsolomm LorimLorim ypsolomm Lorim Lorim ypsolomm Lorim" 
+        ></Article>
+
+        <Article 
+        title="Publicações 3"
+        description=" Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim
+        Lorim ypsolomm Lorim Lorim ypsolomm LorimLorim ypsolomm Lorim Lorim ypsolomm Lorim" 
+        ></Article>
+
+        <Article 
+        title="Publicações 4"
+        description=" Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim Lorim ypsolomm Lorim
+        Lorim ypsolomm Lorim Lorim ypsolomm LorimLorim ypsolomm Lorim Lorim ypsolomm Lorim" 
+        ></Article>
+
+        <Article 
+        title="Para ver outros textos dos nossos alunos acesse :"
+        description=" LINK " 
+        ></Article>
+
+       
+
+        <FaleConosco />
+      <Footer/>
     </div>
   )
 }
