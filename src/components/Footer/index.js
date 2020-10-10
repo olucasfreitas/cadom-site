45aec5bd3cc396b363595f428b4bad762ce8c7a5
david1001cabral@hotmail.com <56028380+davidscabral02@users.noreply.github.com>
2020-10-10 16:17:39 -0300

    Fix: Nomes de componentes e Pastas corrigidos, site pronto pra deploy

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles.css';
 
-function Footer(){
-    return(
-        <footer className="footer-gestao">
-        <p className="textFooter-gestao">CADOM - Centro Acadêmico de Direito Otávio Mendonça - 2020</p>
+function Footer() {
+  return (
+    <footer className="footerContainer">
+      <p className="textFooter">CADOM - Centro Acadêmico de Direito Otávio Mendonça - 2020</p>
 
-        <p className="textFooter-gestao">Desenvolvido por RIT - Rede de Talentos e Idéias</p>
-      </footer>
-    );
+      <p className="textFooter">Desenvolvido por RIT - Rede de Talentos e Idéias</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/pages/Publicacoes/index.js
+++ src/pages/Publicacoes/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import NavBar from '../../components/NavBar/index';
 import './styles.css';
 import Footer from '../../components/Footer/index';
-import ArticleReduced from '../../components/Articel-reduced/index';
+import ArticleReduced from '../../components/ArticelReduced/index';
 import './styles.css';
-import FaleConosco from '../../components/Fale-conosco/index.js';
+import FaleConosco from '../../components/FaleConosco/index.js';
 
 export default function Publicacoes() {
   return (
-
     <>
       <NavBar
         titlePageNav="Publicações"
@@ -17,31 +16,31 @@ export default function Publicacoes() {
         NextPage3="FAQ"
         NextPage4="Gestao" />
 
-      <div className="container-publi">
-        <h1 className="title-page-publi"> Publicações Acadêmicas </h1>
-        <p className="publi-text">
-          Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem
-          Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum ypsolum Lor
+      <div className="containerPubli">
+
+        <h1 className="titlePagePubli"> Publicações Acadêmicas </h1>
+        <p className="publiText">
+          Este espaço será destinado à qualquer estudante do Curso de Direito do CESUPA que queira divulgar
+          suas produções acadêmicas, com o intuito criarmos o nosso próprio acervo digital de Artigos Científicos,
+          TCCs, Teses de Mestrado e Doutorado, etc. Pensamos em criar um espaço para a aquisição e disseminação de conhecimento
+          entre todos os estudantes. Então vamos construir isso tudo juntos!
         </p>
 
         <ArticleReduced
-        title = "Publicação 1"
-        text ="Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem
-        Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum ypsolum Lor"
+          title="Publicação 1"
+          text="O PAPEL DA INTELIGÊNCIA ARTIFICIAL NO SETOR JURÍDICO: O PAPEL DO ROBÔ JUIZ"
+          autor="Autor: Luis Gustavo Savedra Dias"
+          orientador="Orientador: Bruno Ferreira Montenegro"
         />
         <ArticleReduced
-        title = "Publicação 2"
-        text ="Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem
-        Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum ypsolum Lor"
+          title="Publicação 2"
+          text="A TECNOLOGIA COMO FORMA DE DEMOCRATIZAÇÃO DO CONHECIMENTO JURÍDICO"
+          autor="Autor: Luis Gustavo Savedra Dias"
+          orientador="Orientador: Amanda Ramalho"
         />
         <ArticleReduced
-        title = "Publicação 3"
-        text ="Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum Lorem
-        Lorem ypsolum Lorem Lorem ypsolum Lorem Lorem ypsolum ypsolum Lor"
-        />
-        <ArticleReduced
-        title = "Para ver mais textos de nossos alunos, acesse o link abaixo."
-        text ="text Link"
+          title="Para ver mais textos de nossos alunos, acesse o link abaixo."
+          textLink="https://drive.google.com/drive/folders/1im78BNyK5urVP6JFqfagvM9G2hTPeiRi?usp=sharing"
         />
 
         <FaleConosco />
